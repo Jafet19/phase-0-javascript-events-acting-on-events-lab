2e2c593b0f633cb91bf97906cfb36f3544bbf234
Jafet19 <kacur99@gmail.com>
2022-08-31 16:23:31 -0500

    commit tree 99e3127420c15e2b096f85b6d09c61beb849e77b

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
+  
+      dodger.style.left = `${left - 1}px`;
+    }
+  });
+
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+// move to the right 
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowRight") {
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
+  
+      dodger.style.left = `${left + 1}px`;
+    }
+  });
+
+  function moveDodgerRight() {
+    const RightNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(RightNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
